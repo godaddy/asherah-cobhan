26e6da078ee3e27e7f76d5b58c63dc6b73639226
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-02-24 15:52:05 -0800

    Fix scripts

--- linux-build-x64.sh
+++ linux-build-x64.sh
@@ -2,7 +2,9 @@
 
 set -x
 
-LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 go build -v -buildmode=c-shared -o output/libasherah-x64.so
+LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -v -buildmode=c-shared -o output/libasherah-x64.so
 
-LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 go build -v -buildmode=c-shared -tags=debugoutput -o output/libasherah-debug-x64.so
+LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -v -buildmode=c-shared -tags=debugoutput -o output/libasherah-debug-x64.so
+
+find output/ | xargs file
 
--- macos-build.sh
+++ macos-build.sh
@@ -10,3 +10,6 @@ LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build -v -buildmo
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -v -buildmode=c-shared -tags=debugoutput -o output/libasherah-debug-x64.dylib
 
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -v -buildmode=c-shared -o output/libasherah-x64.dylib
+
+find output/ | xargs file
+
