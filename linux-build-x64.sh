d24e7b7bf5f29814d3830bc7f048909fbf974be4
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-02-24 15:46:25 -0800

    Update linux-build-x64.sh

--- linux-build-x64.sh
+++ linux-build-x64.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 go build -v -buildmode=c-shared -o output/libasherah-x64.so
 
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 go build -v -buildmode=c-shared -tags=debugoutput -o output/libasherah-debug-x64.so
