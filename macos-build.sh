8129f28e86769b9a75aba066df89322f04b56127
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-02-24 16:33:41 -0800

    Add header files and release artifacts

--- macos-build.sh
+++ macos-build.sh
@@ -3,13 +3,16 @@
 set -x
 
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build -v -buildmode=c-shared -tags=debugoutput -o output/libasherah-debug-arm64.dylib
+mv output/libasherah.h output/libasherah-debug-darwin-arm64.h
 
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build -v -buildmode=c-shared -o output/libasherah-arm64.dylib
-
+mv output/libasherah.h output/libasherah-darwin-arm64.h
 
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -v -buildmode=c-shared -tags=debugoutput -o output/libasherah-debug-x64.dylib
+mv output/libasherah.h output/libasherah-debug-darwin-x64.h
 
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -v -buildmode=c-shared -o output/libasherah-x64.dylib
+mv output/libasherah.h output/libasherah-darwin-x64.h
 
 find output/ | xargs file
 
--- ubuntu-build.sh
+++ ubuntu-build.sh
@@ -5,12 +5,16 @@ set -x
 sudo apt-get install gcc make gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu
 
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -v -buildmode=c-shared -o output/libasherah-x64.so
+mv output/libasherah.h output/libasherah-x64.h
 
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -v -buildmode=c-shared -tags=debugoutput -o output/libasherah-debug-x64.so
+mv output/libasherah.h output/libasherah-debug-x64.h
 
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=linux GOARCH=arm64 CC=aarch64-linux-gnu-gcc go build -v -buildmode=c-shared -o output/libasherah-arm64.so
+mv output/libasherah.h output/libasherah-arm64.h
 
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=linux GOARCH=arm64 CC=aarch64-linux-gnu-gcc go build -v -buildmode=c-shared -tags=debugoutput -o output/libasherah-debug-arm64.so
+mv output/libasherah.h output/libasherah-debug-arm64.h
 
 find output/ | xargs file
 
