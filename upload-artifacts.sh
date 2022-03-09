e7cf8fce81c0bce6fbbca3d5ae7ff24e4f377f16
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-03-09 08:56:12 -0800

    Fix script

--- upload-artifacts.sh
+++ upload-artifacts.sh
@@ -8,4 +8,4 @@ if [ -n "${GITHUB_TOKEN}" ]; then
     gh auth login --with-token <.githubtoken
     rm .githubtoken
 fi
-gh release upload "${VERSION}" $1 $2 $3 $4 $5 $6 $7 $8 --clobber
+gh release upload "${VERSION}" ${1-} ${2-} ${3-} ${4-} ${5-} ${6-} ${7-} ${8-} --clobber
