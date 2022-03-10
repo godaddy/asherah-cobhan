da036d987212ae0a1c98cf9c335a51eba2f135d3
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-03-10 11:13:05 -0800

    Fix scripts

--- macos-build.sh
+++ macos-build.sh
@@ -8,7 +8,7 @@ mv output/libasherah-arm64.h output/libasherah-darwin-arm64.h
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -v -buildmode=c-shared -o output/libasherah-x64.dylib
 mv output/libasherah-x64.h output/libasherah-darwin-x64.h
 
-go test -v -p 1 -coverprofile cover.out
+go test -v -fastfail -coverprofile cover.out
 
-find output/ | xargs file
+find output/ -print0 | xargs -0 file
 
--- ubuntu-build-arm64.sh
+++ ubuntu-build-arm64.sh
@@ -6,4 +6,4 @@ apt-get install gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu -y
 
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=linux GOARCH=arm64 CC=aarch64-linux-gnu-gcc go build -v -buildmode=c-shared -o output/libasherah-arm64.so
 
-find output/ | xargs file
+find output/ -print0 | xargs -0 file
