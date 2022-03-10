3b1808b5bdf05e3860180f7e43c2ba67ac843567
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-03-10 11:14:35 -0800

    Fix bad parameter name

--- macos-build.sh
+++ macos-build.sh
@@ -8,7 +8,7 @@ mv output/libasherah-arm64.h output/libasherah-darwin-arm64.h
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -v -buildmode=c-shared -o output/libasherah-x64.dylib
 mv output/libasherah-x64.h output/libasherah-darwin-x64.h
 
-go test -v -fastfail -coverprofile cover.out
+go test -v -failfast -coverprofile cover.out
 
 find output/ -print0 | xargs -0 file
 
--- ubuntu-build-x64.sh
+++ ubuntu-build-x64.sh
@@ -4,7 +4,7 @@ set -xeu
 
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -v -buildmode=c-shared -o output/libasherah-x64.so
 
-go test -v -fastfail -coverprofile cover.out
+go test -v -failfast -coverprofile cover.out
 
 find output/ -print0 | xargs -0 file
 
