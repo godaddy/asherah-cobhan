e5f0177ea5e9640c1638bbd963d39890254ffa6e
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-03-14 13:13:56 -0700

    Fix v for regex

--- upload-artifacts.sh
+++ upload-artifacts.sh
@@ -4,7 +4,7 @@ set -xeu
 
 VERSION=${GITHUB_REF#refs/*/}
 
-if [[ ${VERSION} =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+if [[ ${VERSION#v} =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   if [ -n "${GITHUB_TOKEN}" ]; then
     echo "${GITHUB_TOKEN}" >.githubtoken
     unset GITHUB_TOKEN
