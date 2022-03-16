67917c39838f67b25b59f1bf7b483c68b67d5672
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-03-16 14:26:29 -0700

    Move constants

---
+++ constants.go
@@ -0,0 +1,12 @@
+package libasherah
+
+const ERR_NOT_INITIALIZED = -100
+const ERR_ALREADY_INITIALIZED = -101
+const ERR_GET_SESSION_FAILED = -102
+const ERR_ENCRYPT_FAILED = -103
+const ERR_DECRYPT_FAILED = -104
+const ERR_BAD_CONFIG = -105
+
+const EstimatedEncryptionOverhead = 48
+const EstimatedEnvelopeOverhead = 185
+const Base64Overhead = 1.34
--- libasherah.go
+++ libasherah.go
@@ -15,17 +15,6 @@ import (
 	"github.com/godaddy/asherah/go/appencryption"
 )
 
-const ERR_NOT_INITIALIZED = -100
-const ERR_ALREADY_INITIALIZED = -101
-const ERR_GET_SESSION_FAILED = -102
-const ERR_ENCRYPT_FAILED = -103
-const ERR_DECRYPT_FAILED = -104
-const ERR_BAD_CONFIG = -105
-
-const EstimatedEncryptionOverhead = 48
-const EstimatedEnvelopeOverhead = 185
-const Base64Overhead = 1.34
-
 var EstimatedIntermediateKeyOverhead = 0
 
 //export Shutdown
