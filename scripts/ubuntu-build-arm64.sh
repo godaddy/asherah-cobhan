336afb01e708155d8ac432e5a892d3712cbec54b
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2022-03-22 14:22:24 +0100

    Set ldflags to optimize the size of released binaries

--- scripts/ubuntu-build-arm64.sh
+++ scripts/ubuntu-build-arm64.sh
@@ -4,6 +4,6 @@ set -xeu
 
 apt-get install gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu -y
 
-LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=linux GOARCH=arm64 CC=aarch64-linux-gnu-gcc go build -v -buildmode=c-shared -o output/libasherah-arm64.so
+LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=linux GOARCH=arm64 CC=aarch64-linux-gnu-gcc go build -v -buildmode=c-shared -ldflags='-s -w' -o output/libasherah-arm64.so
 
 find output/ -print0 | xargs -0 file
