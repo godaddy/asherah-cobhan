88e120903835dc6a3a57be06709f8d527f8874d4
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-03-21 20:55:15 -0700

    Attempt to tune with -O3 and amd64 v2 set

--- scripts/macos-build.sh
+++ scripts/macos-build.sh
@@ -2,10 +2,10 @@
 
 set -xeu
 
-LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GODEBUG=cgocheck=0 GOOS=darwin GOARCH=arm64 go build -v -buildmode=c-shared -o output/libasherah-arm64.dylib
+LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GODEBUG=cgocheck=0 GO_CFLAGS=-O3 GOOS=darwin GOARCH=arm64 go build -v -buildmode=c-shared -o output/libasherah-arm64.dylib
 mv output/libasherah-arm64.h output/libasherah-darwin-arm64.h
 
-LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GODEBUG=cgocheck=0 GOOS=darwin GOARCH=amd64 go build -v -buildmode=c-shared -o output/libasherah-x64.dylib
+LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GODEBUG=cgocheck=0 GO_CFLAGS=-O3 GOAMD64=v2 GOOS=darwin GOARCH=amd64 go build -v -buildmode=c-shared -o output/libasherah-x64.dylib
 mv output/libasherah-x64.h output/libasherah-darwin-x64.h
 
 go test -v -failfast -coverprofile cover.out
