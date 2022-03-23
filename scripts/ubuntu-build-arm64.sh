b8f2baf5b4f8f969d39c16aed7378962acddd4e6
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-03-22 19:05:41 -0700

    Remove PIC

--- scripts/ubuntu-build-arm64.sh
+++ scripts/ubuntu-build-arm64.sh
@@ -4,7 +4,7 @@ set -xeu
 
 apt-get install gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu -y
 
-CGO_ENABLED=1 GOOS=linux GODEBUG=cgocheck=0 GOARCH=arm64 CC=aarch64-linux-gnu-gcc go build -v -buildmode=c-archive -ldflags='-fPIC -s -w' -o output/libasherah-arm64.a
+CGO_ENABLED=1 GOOS=linux GODEBUG=cgocheck=0 GOARCH=arm64 CC=aarch64-linux-gnu-gcc go build -v -buildmode=c-archive -ldflags='-s -w' -o output/libasherah-arm64.a
 mv output/libasherah-arm64.h output/libasherah-arm64-archive.h
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GODEBUG=cgocheck=0 GOOS=linux GOARCH=arm64 CC=aarch64-linux-gnu-gcc go build -v -buildmode=c-shared -ldflags='-s -w' -o output/libasherah-arm64.so
 
--- scripts/ubuntu-build-x64.sh
+++ scripts/ubuntu-build-x64.sh
@@ -2,7 +2,7 @@
 
 set -xeu
 
-CGO_ENABLED=1 GOOS=linux GODEBUG=cgocheck=0 GOARCH=amd64 go build -v -buildmode=c-archive -ldflags='-fPIC -s -w' -o output/libasherah-x64.a
+CGO_ENABLED=1 GOOS=linux GODEBUG=cgocheck=0 GOARCH=amd64 go build -v -buildmode=c-archive -ldflags='-s -w' -o output/libasherah-x64.a
 mv output/libasherah-x64.h output/libasherah-x64-archive.h
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=linux GODEBUG=cgocheck=0 GOARCH=amd64 go build -v -buildmode=c-shared -ldflags='-s -w' -o output/libasherah-x64.so
 
