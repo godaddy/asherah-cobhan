8eb1f9c387eef4a0b888263a914a6f6f20e382b1
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-03-22 18:41:04 -0700

    Try to build c-archive files as well

--- scripts/ubuntu-build-x64.sh
+++ scripts/ubuntu-build-x64.sh
@@ -3,6 +3,7 @@
 set -xeu
 
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=linux GODEBUG=cgocheck=0 GOARCH=amd64 go build -v -buildmode=c-shared -ldflags='-s -w' -o output/libasherah-x64.so
+CGO_ENABLED=1 GOOS=linux GODEBUG=cgocheck=0 GOARCH=amd64 go build -v -buildmode=c-archive -ldflags='-s -w' -o output/libasherah-x64.a
 
 go test -v -failfast -coverprofile cover.out
 
