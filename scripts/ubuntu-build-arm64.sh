9948099c8d050abb8bb1e2a7690a7e3c2f8b06ec
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-03-22 19:01:48 -0700

    Make Archives PIC and save headers

--- scripts/ubuntu-build-arm64.sh
+++ scripts/ubuntu-build-arm64.sh
@@ -4,6 +4,8 @@ set -xeu
 
 apt-get install gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu -y
 
+CGO_ENABLED=1 GOOS=linux GODEBUG=cgocheck=0 GOARCH=arm64 CC=aarch64-linux-gnu-gcc go build -v -buildmode=c-archive -ldflags='-fPIC -s -w' -o output/libasherah-arm64.a
+mv output/libasherah-arm64.h output/libasherah-arm64-archive.h
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GODEBUG=cgocheck=0 GOOS=linux GOARCH=arm64 CC=aarch64-linux-gnu-gcc go build -v -buildmode=c-shared -ldflags='-s -w' -o output/libasherah-arm64.so
-CGO_ENABLED=1 GOOS=linux GODEBUG=cgocheck=0 GOARCH=arm64 CC=aarch64-linux-gnu-gcc go build -v -buildmode=c-archive -ldflags='-s -w' -o output/libasherah-arm64.a
+
 find output/ -print0 | xargs -0 file
--- scripts/ubuntu-build-x64.sh
+++ scripts/ubuntu-build-x64.sh
@@ -2,8 +2,9 @@
 
 set -xeu
 
+CGO_ENABLED=1 GOOS=linux GODEBUG=cgocheck=0 GOARCH=amd64 go build -v -buildmode=c-archive -ldflags='-fPIC -s -w' -o output/libasherah-x64.a
+mv output/libasherah-x64.h output/libasherah-x64-archive.h
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GOOS=linux GODEBUG=cgocheck=0 GOARCH=amd64 go build -v -buildmode=c-shared -ldflags='-s -w' -o output/libasherah-x64.so
-CGO_ENABLED=1 GOOS=linux GODEBUG=cgocheck=0 GOARCH=amd64 go build -v -buildmode=c-archive -ldflags='-s -w' -o output/libasherah-x64.a
 
 go test -v -failfast -coverprofile cover.out
 
