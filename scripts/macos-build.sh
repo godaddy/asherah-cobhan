6576f728a861491888a7844a155c1c7c6ef6da3d
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-03-24 11:19:18 -0700

    Fix names

--- scripts/macos-build.sh
+++ scripts/macos-build.sh
@@ -2,12 +2,12 @@
 
 set -xeu
 
-CGO_ENABLED=1 GOOS=darwin GODEBUG=cgocheck=0 GOARCH=arm64 go build -v -buildmode=c-archive -ldflags='-s -w' -o output/libasherah-arm64.a
+CGO_ENABLED=1 GOOS=darwin GODEBUG=cgocheck=0 GOARCH=arm64 go build -v -buildmode=c-archive -ldflags='-s -w' -o output/libasherah-darwin-arm64.a
 mv output/libasherah-arm64.h output/libasherah-darwin-arm64-archive.h
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GODEBUG=cgocheck=0 GOOS=darwin GOARCH=arm64 go build -v -buildmode=c-shared -ldflags='-s -w' -o output/libasherah-arm64.dylib
 mv output/libasherah-arm64.h output/libasherah-darwin-arm64.h
 
-CGO_ENABLED=1 GOOS=darwin GODEBUG=cgocheck=0 GOARCH=amd64 go build -v -buildmode=c-archive -ldflags='-s -w' -o output/libasherah-x64.a
+CGO_ENABLED=1 GOOS=darwin GODEBUG=cgocheck=0 GOARCH=amd64 go build -v -buildmode=c-archive -ldflags='-s -w' -o output/libasherah-darwin-x64.a
 mv output/libasherah-x64.h output/libasherah-darwin-x64-archive.h
 LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 GODEBUG=cgocheck=0 GOOS=darwin GOARCH=amd64 go build -v -buildmode=c-shared -ldflags='-s -w' -o output/libasherah-x64.dylib
 mv output/libasherah-x64.h output/libasherah-darwin-x64.h
