df201823191501999893ed71f76e35b5a21f4e28
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2022-09-10 18:42:33 +0200

    Fix gh install

--- scripts/ubuntu-configure.sh
+++ scripts/ubuntu-configure.sh
@@ -4,11 +4,12 @@ set -xeu
 # Optionally use a different mirror if azure.archive.ubuntu.com is being lame
 #sed -i 's/azure.archive.ubuntu.com/mirror.arizona.edu/g' /etc/apt/sources.list
 apt-get update -y
-apt-get install software-properties-common -y
+apt-get install software-properties-common curl -y
 
-apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-#echo "deb https://cli.github.com/packages bullseye main" >> /etc/apt/sources.list
-apt-add-repository https://cli.github.com/packages
+# https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
 apt-get update -y
 
